300b29a3f751376d7eb7b75b0c31102a7e91cee3
Riya <riyadavis897@gmail.com>
2019-08-07 15:04:46 +0400

    Pusher Added

--- application/views/pages/chat.php
+++ application/views/pages/chat.php
@@ -17,7 +17,7 @@
 
         var channel = pusher.subscribe('my-channel');
         channel.bind('my-event', function(data) {
-        alert(JSON.stringify(data));
+            console.log(data);
         });
     </script>    
 </head>
