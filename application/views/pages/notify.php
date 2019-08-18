4d5482c4ea1a21d1d2bb9ea5520cc972afca5424
Riya <riyadavis897@gmail.com>
2019-08-18 22:31:05 +0400

    token

--- application/views/pages/notify.php
+++ application/views/pages/notify.php
@@ -24,7 +24,7 @@
 </head>
 <body>
 <div class="container">
-    <div id="chatBox" style="background-color:white;width:40%;min-height:300px">
+    <div id="chatBox" style="background-color:wheat;width:40%;min-height:300px">
     </div>
     <div>
         <form action="" method="post">
@@ -37,6 +37,7 @@
     var url = "<?php echo site_url('MyWebsiteApi/notification'); ?>";
     $('form').submit(function(e){
         e.preventDefault();
+        document.getElementById('chatbox').innerHTML = "";
         ajaxPost();
         async function ajaxPost()
         {
