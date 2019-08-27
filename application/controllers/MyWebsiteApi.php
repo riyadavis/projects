80743fac7c65708b3325245d06138d3faf4cb616
Riya <riyadavis897@gmail.com>
2019-08-27 23:12:14 +0400

    order

--- application/controllers/MyWebsiteApi.php
+++ application/controllers/MyWebsiteApi.php
@@ -66,24 +66,24 @@ class MyWebsiteApi extends CI_Controller
     }
     public function confirmMessage()
     {
-        // $this->load->third_party('Pusher');
-        // $this->Pusher->pusher();
+        $this->load->third_party('Pusher');
+        $this->Pusher->pusher();
         // $this->load->view('confirm');
-        require __DIR__ .'vendor/autoload.php';
+        // require __DIR__ .'vendor/autoload.php';
 
-        $options = array(
-        'cluster' => 'ap2',
-        'useTLS' => true
-        );
-        $pusher = new Pusher\Pusher(
-        'e6256b34427ca9b29815',
-        'e1a37e8c0910ae055d3b',
-        '838370',
-        $options
-        );
+        // $options = array(
+        // 'cluster' => 'ap2',
+        // 'useTLS' => true
+        // );
+        // $pusher = new Pusher\Pusher(
+        // 'e6256b34427ca9b29815',
+        // 'e1a37e8c0910ae055d3b',
+        // '838370',
+        // $options
+        // );
 
-        $data['message'] = 'hello world';
-        $pusher->trigger('my-channel', 'my-event', $data);
+        // $data['message'] = 'hello world';
+        // $pusher->trigger('my-channel', 'my-event', $data);
   
 
     }
