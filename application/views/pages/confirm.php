b49f4d75fff1c1381d71f18813b453aa66b11323
Riya <riyadavis897@gmail.com>
2019-08-27 23:15:46 +0400

    order

--- application/views/pages/confirm.php
+++ application/views/pages/confirm.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
+<html>
 <head>
   <title>Pusher Test</title>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>    
   <script src="https://js.pusher.com/5.0/pusher.min.js"></script>
   <script>
 
